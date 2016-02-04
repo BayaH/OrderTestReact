cf2b95d6a4d9ffd1e0e8cad2a844231d3411d6f6
HAMIDI Baya <baya.hamidi@sword-group.com>
2016-02-04 14:00:25 +0100

    init app order

---
+++ app/components/CreateOrder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CreateOrder extends React.Component {
+  render() {
+    return (
+      <div>
+        //Tabs for order
+      </div>
+    );
+  }
+}
